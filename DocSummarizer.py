f96e8fce6ad57983b21fc638471a478d36120a7f
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-16 19:19:26 -0700

    Update DocSummarizer.py

--- DocSummarizer.py
+++ DocSummarizer.py
@@ -11,18 +11,23 @@ def summarize_document(document):
       return summarized_text
 
 st.title("Document Summarizer")
-summarize_button=st.button("Summarize")
 upload_button=st.button("UploadFile")
+summarize_button=st.button("Summarize")
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
 if upload_button and openai_api_key.startswith('sk-'):
     uploaded_file = st.file_uploader("Upload a document file", type=["txt", "pdf"])
     if uploaded_file is not None:
+        print("Inside the upload_file if loop")
         st.write("### Uploaded Document Contents:")
         file_contents = uploaded_file.read().decode("utf-8")
+        print("File Contets are read")
         st.write(file_contents)
         if summarize_button:
            summarized_text = summarize_document(file_contents)
            st.write("### Summarized Document:")
            st.write(summarized_text)
+      else
+         print("In the else function")
+
