d24ad6eb1c7800d1194f654d5140048cbf658c0c
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-16 19:35:13 -0700

    Update DocSummarizer.py

--- DocSummarizer.py
+++ DocSummarizer.py
@@ -4,28 +4,42 @@ from langchain.llms import OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 
-def summarize_document(document):
-      prompt = ChatPromptTemplate.from_template(document)
-      response = prompt | openai.Completion.create(engine="text-davinci-002", max_tokens=150)
-      summarized_text = response.choices[0].text.strip()
-      return summarized_text
+#def summarize_document(document):
+ #     prompt = ChatPromptTemplate.from_template(document)
+  #    response = prompt | openai.Completion.create(engine="text-davinci-002", max_tokens=150)
+   #   summarized_text = response.choices[0].text.strip()
+    #  return summarized_text
 
-st.title("Document Summarizer")
-summarize_button=st.button("Summarize")
-openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+#st.title("Document Summarizer")
+#summarize_button=st.button("Summarize")
+#openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 #if not openai_api_key.startswith('sk-'):
     #st.warning('Please enter your OpenAI API key!', icon='⚠')
-if summarize_button and openai_api_key.startswith('sk-'):
-    print("uploading a file")  
-    uploaded_file = st.file_uploader("Upload a document file", type=["txt", "pdf"])
-    if uploaded_file is not None:
-        print("Inside the upload_file if loop")
-        st.write("### Uploaded Document Contents:")
-        file_contents = uploaded_file.read().decode("utf-8")
-        print("File Contets are read")
-        st.write(file_contents)
-        if summarize_button:
-           summarized_text = summarize_document(file_contents)
-           st.write("### Summarized Document:")
-           st.write(summarized_text)
+#if summarize_button and openai_api_key.startswith('sk-'):
+ #   print("uploading a file")  
+  #  uploaded_file = st.file_uploader("Upload a document file", type=["txt", "pdf"])
+   # if uploaded_file is not None:
+    #    print("Inside the upload_file if loop")
+     #   st.write("### Uploaded Document Contents:")
+      #  file_contents = uploaded_file.read().decode("utf-8")
+       # print("File Contets are read")
+        #st.write(file_contents)
+       # if summarize_button:
+        #   summarized_text = summarize_document(file_contents)
+         #  st.write("### Summarized Document:")
+          # st.write(summarized_text)
 
+
+st.title("Text File Uploader")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
+
+# Display file contents
+if uploaded_file is not None:
+    # Read file contents
+    file_contents = uploaded_file.read().decode("utf-8")
+
+    # Display file contents
+    st.write("### Uploaded File Contents:")
+    st.write(file_contents)
