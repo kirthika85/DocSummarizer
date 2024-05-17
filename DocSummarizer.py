5cdfb481b9712bbd15e6544d70591be2ddfab3fd
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-16 19:48:39 -0700

    Update DocSummarizer.py

--- DocSummarizer.py
+++ DocSummarizer.py
@@ -13,19 +13,19 @@ from langchain_core.prompts import ChatPromptTemplate
 st.title("Document Summarizer")
 upload_button=st.button("UploadFile")
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
+#if not openai_api_key.startswith('sk-'):
+   # st.warning('Please enter your OpenAI API key!', icon='⚠')
 if upload_button and openai_api_key.startswith('sk-'):
    uploaded_file = st.file_uploader("Upload a text file", type=["txt"])
    if uploaded_file is not None:
       st.write("### Uploaded Document Contents:")
       file_contents = uploaded_file.read().decode("utf-8")
       st.write(file_contents)
-      summarize_button=st.button("Summarize")   
-      if summarize_button:
-         prompt=ChatPromptTemplate.from_template(file_contents)
-         response=prompt|openai.Completion.create(engine="text-davinci-002", max_tokens=150)
+      #summarize_button=st.button("Summarize")   
+      #if summarize_button:
+       #  prompt=ChatPromptTemplate.from_template(file_contents)
+        # response=prompt|openai.Completion.create(engine="text-davinci-002", max_tokens=150)
          #summarized_text = summarize_document(file_contents)
-         summarized_text = response.choices[0].text.strip()
-         st.write("### Summarized Document:")
-         st.write(summarized_text)
+         #summarized_text = response.choices[0].text.strip()
+         #st.write("### Summarized Document:")
+         #st.write(summarized_text)
